81e4792ec41da3a900237495f1800d68687fa0f5
Thales Gomes Targino <thalestargino@gmail.com>
2022-05-03 17:11:44 -0300

    feat: formatter, linter and react template

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import ContainerApp from './Layout/App';
+
+function App() {
+    return (
+        <ContainerApp>
+            <h1>Hello World</h1>
+        </ContainerApp>
+    );
+}
+
+export default App;
---
+++ src/components/Layout/App.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ContainerApp = styled.div`
+    h1 {
+        color: #fa0000;
+    }
+`;
+
+export default ContainerApp;
